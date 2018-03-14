9f3f06d5e3ddef4500d4f696ef9a64b8d056fe66
Colin-Shaw2 <colin.shaw2@uoit.net>
2018-03-14 12:53:28 -0400

    Fixed path names and fixed maki roll constructor

--- src/Cards/Card.java
+++ src/Cards/Card.java
@@ -29,73 +29,101 @@ public abstract class Card{
 
 
         public Chopsticks() throws IOException {
-            super("Chop Sticks", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Chopsticks", ImageIO.read(new File("../CardImages/Chopsticks.jpg")));
         }
     }
+
 
     public class Dumpling extends Card{
 
 
         public Dumpling() throws IOException {
-            super("Dumpling", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Dumpling", ImageIO.read(new File("../CardImages/Dumpling.jpg")));
         }
     }
+
 
     public class EggNigiri extends Card{
 
 
         public EggNigiri() throws IOException {
-            super("Egg Nigiri", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Egg Nigiri", ImageIO.read(new File("../CardImages/Egg_Nigiri.jpg")));
         }
     }
+
+
     public class MakiRoll extends Card{
         int num;
 
-        public MakiRoll() throws IOException {
-            super("Maki Rolls", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+        public MakiRoll(int num) throws IOException {
+            super("Maki Rolls", null);
             this.num = num;
+            if(num == 1){
+                cardImage = ImageIO.read(new File("../CardImages/Maki_Roll_1.jpg"));
+            }
+            else if(num == 2){
+                ImageIO.read(new File("../CardImages/Maki_Roll_2.jpg"));
+            }
+            else if(num == 3){
+                ImageIO.read(new File("../CardImages/Maki_Roll_3.jpg"));
+            }
+            else{
+                System.err.print("Invalid Maki Roll Number must be 1-3\nValue passed was " + num);
+            }
         }
     }
+
+
     public class Pudding extends Card{
 
 
         public Pudding() throws IOException {
-            super("Chop Sticks", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Pudding", ImageIO.read(new File("../CardImages/Pudding.jpg")));
         }
     }
+
+
     public class SalmonNigiri extends Card{
 
 
         public SalmonNigiri() throws IOException {
-            super("Salmon Nigiri", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Salmon Nigiri", ImageIO.read(new File("../CardImages/Salmon_Nigiri.jpg")));
         }
     }
+
+
     public class Sashimi extends Card{
 
 
         public Sashimi() throws IOException {
-            super("Sashimi", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Sashimi", ImageIO.read(new File("../CardImages/Sashimi.jpg")));
         }
     }
+
+
     public class SquidNigiri extends Card{
 
 
         public SquidNigiri() throws IOException {
-            super("Squid Nigiri", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Squid Nigiri", ImageIO.read(new File("../CardImages/Squid_Nigiri.jpg")));
         }
     }
+
+
     public class Tempura extends Card{
 
 
         public Tempura() throws IOException {
-            super("Tempura", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Tempura", ImageIO.read(new File("../CardImages/Tempura.jpg")));
         }
     }
+
+
     public class Wasabi extends Card{
         private boolean used;
 
         public Wasabi() throws IOException {
-            super("Wasabi", ImageIO.read(new File("../CardImages/Cardback.jpg")));
+            super("Wasabi", ImageIO.read(new File("../CardImages/Wasabi.jpg")));
             used = false;
         }
     }
