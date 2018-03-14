c93847c3511d08a3a41e14d0219bb27e65dd8857
jon-perry <jonathan.perry.17@gmail.com>
2018-03-14 12:45:31 -0400

    game folder and deck class added

---
+++ src/Game/Deck.java
@@ -0,0 +1,15 @@
+package Game;
+
+import Cards.Card;
+import java.util.*;
+
+public class Deck {
+    Stack<Card> deck;
+
+    Deck(){
+        // create an arraylist, shuffle it, the populate the stack
+        ArrayList<Card> tmp;
+
+        }
+    }
+}
