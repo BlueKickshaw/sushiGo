3dc4245eafbef08d6f997d485ca888a8b0450959
Colin-Shaw2 <colin.shaw2@uoit.net>
2018-03-14 15:23:20 -0400

    Extended funtionality to calculate points

--- src/Game/GameDriver.java
+++ src/Game/GameDriver.java
@@ -7,37 +7,54 @@ import java.util.Vector;
 public class GameDriver {
 
     //create the hands
-    static Vector<Hand> playerHands;
-    static Vector<Hand> rotatingHands;
+    private static Vector<Player> playerList;
+    private static int numOfPlayers;
 
-    public void playGame(){
+    public void initialize(int numOfPlayers){
         //choose players and hand size
-        int numOfPlayers = 4;
+        numOfPlayers = 4;
         int handSize = 8;
 
-        playerHands = new Vector<>(handSize);
-        rotatingHands = new Vector<>(handSize);
+        playerList = new Vector<>(numOfPlayers);
         //create a deck
         Deck deck = new Deck();
 
-
-        int[] players = {0,1,2,3};
 
 
 
         //deal the hands
         for (int i = 0; i <numOfPlayers; i++) {
-            rotatingHands.get(i).addCard(deck.deal());
+            for (int j = 0; j < handSize; j++) {
+                playerList.get(i).getPlayerHand().addCard(deck.deal());
+            }
         }
     }
 
-    public void chooseCard(int playerNum, Card c){
-        playerHands.get(playerNum).removeCard(c);
-        rotatingHands.get(playerNum).addCard(c);
+    public void chooseCard(Player player, Card c){
+        player.chooseCard(c);
     }
 
-    public int calculatePoints(){}
+    public int[] calculatePoints(Vector<Player> playerList){
+        int points =0;
+        for (int i = 0; i < numOfPlayers; i++) {
+            Player currentPLayer = playerList.get(i);
+            points += currentPLayer.dumplingPoints();
+            points += currentPLayer.makiRollPoints(playerList);
+            points += currentPLayer.nigiriPoints();
+            points += currentPLayer.sashimiPoints();
+            points += currentPLayer.tempuraPoints();
+            currentPLayer.addPoints(points);
+        }
+    }
 
-    public int calulatePuddingPoints(){}
+    public int calulatePuddingPoints(Vector<Player> playerList){
+        int points =0;
+        int indexOfHighest;
+        for (int i = 0; i < numOfPlayers; i++) {
+            playerList.get(i).getPudding();//TODO
+        }
+
+        return 0;
+    }
 
 }
