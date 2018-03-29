fd2a9b6aa62fb1108e674b5a52f221e34cd8d8a4
jon-perry <jonathan.perry.17@gmail.com>
2018-03-29 13:30:01 -0400

    UI updating correctly for all players (cardbacks), also added a rotated cardback image to CardImages

--- src/Game/FourPlayerController.java
+++ src/Game/FourPlayerController.java
@@ -37,7 +37,63 @@ public class FourPlayerController {
     @FXML
     ImageView playerCard07 = new ImageView();
     @FXML
-    static Vector<ImageView> imageViews = new Vector<>();
+    static Vector<ImageView> playerCardImages = new Vector<>();
+    @FXML
+    ImageView opponent0Card00 = new ImageView();
+    @FXML
+    ImageView opponent0Card01 = new ImageView();
+    @FXML
+    ImageView opponent0Card02 = new ImageView();
+    @FXML
+    ImageView opponent0Card03 = new ImageView();
+    @FXML
+    ImageView opponent0Card04  = new ImageView();
+    @FXML
+    ImageView opponent0Card05  = new ImageView();
+    @FXML
+    ImageView opponent0Card06  = new ImageView();
+    @FXML
+    ImageView opponent0Card07  = new ImageView();
+    Vector<ImageView> opponent0CardBacks = new Vector<>();
+    @FXML
+    ImageView leftOpponent0Card00 = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card01 = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card02 = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card03 = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card04  = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card05  = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card06  = new ImageView();
+    @FXML
+    ImageView leftOpponent0Card07  = new ImageView();
+    Vector<ImageView> leftOpponent0CardBacks = new Vector<>();
+    @FXML
+    ImageView rightOpponent0Card00 = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card01 = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card02 = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card03 = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card04  = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card05  = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card06  = new ImageView();
+    @FXML
+    ImageView rightOpponent0Card07  = new ImageView();
+    Vector<ImageView> rightOpponent0CardBacks = new Vector<>();
+
+
+    Image cardBack = new Image("/Game/CardImages/Cardback.jpg");
+    Image rotatedCardBack = new Image ("/Game/CardImages/Cardback_Rotated.jpg");
+
     Player player = new Player("Jon", "123");
     Deck deck = new Deck();
     int roundCount = 0;
@@ -50,12 +106,14 @@ public class FourPlayerController {
     }
 
 
-
     public void incrementRound(ActionEvent event) {
 
         player.getRotatingHand().setCards(deck.drawCards(8 - roundCount));
-        populateImages();
-
+        populateImages(playerCardImages);
+        populateCardBacks(opponent0CardBacks, cardBack);
+        populateCardBacks(leftOpponent0CardBacks, rotatedCardBack);
+        populateCardBacks(rightOpponent0CardBacks, rotatedCardBack);
+//        leftOpponent0CardBacks.get(1).setImage(cardBack);
         // re-enable all buttons to that will be populated by card images this round
         enableAppropriateButtons();
         roundCount++;
@@ -71,14 +129,40 @@ public class FourPlayerController {
 
     public void initialize() {
 
-        imageViews.add(playerCard00);
-        imageViews.add(playerCard07);
-        imageViews.add(playerCard01);
-        imageViews.add(playerCard06);
-        imageViews.add(playerCard05);
-        imageViews.add(playerCard02);
-        imageViews.add(playerCard04);
-        imageViews.add(playerCard03);
+        playerCardImages.add(playerCard00);
+        playerCardImages.add(playerCard07);
+        playerCardImages.add(playerCard01);
+        playerCardImages.add(playerCard06);
+        playerCardImages.add(playerCard05);
+        playerCardImages.add(playerCard02);
+        playerCardImages.add(playerCard04);
+        playerCardImages.add(playerCard03);
+        opponent0CardBacks.add(opponent0Card00);
+        opponent0CardBacks.add(opponent0Card07);
+        opponent0CardBacks.add(opponent0Card01);
+        opponent0CardBacks.add(opponent0Card06);
+        opponent0CardBacks.add(opponent0Card05);
+        opponent0CardBacks.add(opponent0Card02);
+        opponent0CardBacks.add(opponent0Card04);
+        opponent0CardBacks.add(opponent0Card03);
+        leftOpponent0CardBacks.add(leftOpponent0Card00);
+        leftOpponent0CardBacks.add(leftOpponent0Card07);
+        leftOpponent0CardBacks.add(leftOpponent0Card01);
+        leftOpponent0CardBacks.add(leftOpponent0Card06);
+        leftOpponent0CardBacks.add(leftOpponent0Card05);
+        leftOpponent0CardBacks.add(leftOpponent0Card02);
+        leftOpponent0CardBacks.add(leftOpponent0Card04);
+        leftOpponent0CardBacks.add(leftOpponent0Card03);
+        rightOpponent0CardBacks.add(rightOpponent0Card00);
+        rightOpponent0CardBacks.add(rightOpponent0Card07);
+        rightOpponent0CardBacks.add(rightOpponent0Card01);
+        rightOpponent0CardBacks.add(rightOpponent0Card06);
+        rightOpponent0CardBacks.add(rightOpponent0Card05);
+        rightOpponent0CardBacks.add(rightOpponent0Card02);
+        rightOpponent0CardBacks.add(rightOpponent0Card04);
+        rightOpponent0CardBacks.add(rightOpponent0Card03);
+
+
         player.drawHand(deck, 4);
 
 
@@ -190,28 +274,40 @@ public class FourPlayerController {
 
 
     public static void disableButtons() {
-        for (ImageView buttonClick : imageViews) {
+        for (ImageView buttonClick : playerCardImages) {
             buttonClick.setDisable(true);
         }
     }
 
     private void enableAppropriateButtons() {
-        for (int i = roundCount; i < imageViews.size(); i++) {
-            imageViews.get(i).setDisable(false);
+        for (int i = roundCount; i < playerCardImages.size(); i++) {
+            playerCardImages.get(i).setDisable(false);
         }
     }
 
-    private void populateImages() {
-        for (ImageView iv : imageViews) {
+    private void populateImages(Vector<ImageView> images) {
+        for (ImageView iv : images) {
             iv.setImage(null);
         }
 
-        Vector<ImageView> reverseImageViews = (Vector) imageViews.clone();
+        Vector<ImageView> reverseImageViews = (Vector) images.clone();
         Collections.reverse(reverseImageViews);
         for (int i = 0; i < player.getRotatingHand().getCards().size(); i++) {
             Card tmp = player.getRotatingHand().getCard(i);
             Image image = new Image(tmp.getImagePath());
             reverseImageViews.get(i).setImage(image);
+        }
+
+    }
+
+    private void populateCardBacks(Vector<ImageView> oppCardsView, Image back){
+        for (ImageView iv : oppCardsView) {
+            iv.setImage(null);
+        }
+        Vector<ImageView> reverseImageViews = (Vector) oppCardsView.clone();
+        Collections.reverse(reverseImageViews);
+        for (int i = 0; i < player.getRotatingHand().getCards().size(); i++) {
+            reverseImageViews.get(i).setImage(back);
         }
 
     }
