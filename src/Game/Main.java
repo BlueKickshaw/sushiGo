00fab82df36df33d9b495e1dbb72626ea0cf4f74
jon-perry <jonathan.perry.17@gmail.com>
2018-03-29 17:13:44 -0400

    Two player finished, had to rework turn to take in ImageViews in order ot properly disable buttons. disabling buttons is no longer apart of a controller and is handled by turn

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("scenes/4PlayerGame.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("scenes/2PlayerGame.fxml"));
         primaryStage.setTitle("sushiGo!");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
